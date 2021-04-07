8cb875fa439587a7a0c5097b6bae3db56a404d2e
NicoGovStudent <69640301+NicoGovStudent@users.noreply.github.com>
2021-04-07 08:41:05 +0200

    insertion scripts for registration

--- Planning/Files/php/insert_admin.php
+++ Planning/Files/php/insert_admin.php
@@ -17,6 +17,7 @@ $phoneNum = $_REQUEST["phoneNum"];
 $email = $_REQUEST["email"];
 $idNum = $_REQUEST["idNum"];
 $password = $_REQUEST["password"];
+$secretKey = $_REQUEST["secretKey"];
 
 $streetName = $_REQUEST["streetName"];
 $streetNum = $_REQUEST["streetNum"];
@@ -25,14 +26,20 @@ $province = $_REQUEST["province"];
 $country = $_REQUEST["country"];
 $apartmentNum = $_REQUEST["apartmentNum"];
 
-$sql1 = "SELECT COUNT(*) AS RESULT FROM CLIENT WHERE idNumber = '$idNum'";
+$sql1 = "SELECT COUNT(*) AS RESULT FROM ADMIN WHERE idNumber = '$idNum'";
+$sql2 = "SELECT COUNT(*) AS RESULT FROM `SECRET KEY` WHERE secretKey = '$secretKey'";
 
-if(mysqli_query($conn, $sql1)) {
-	$check_count = mysqli_fetch_array($check);
-    if($check_count['RESULT'] != '0'){
+$check1 = mysqli_query($conn, $sql1);
+$check2 = mysqli_query($conn, $sql2);
+if($check1 and $check2) {
+	$check_admin = mysqli_fetch_array($check1);
+	$check_secret_key = mysqli_fetch_array($check2);
+    if($check_admin['RESULT'] != '0'){
 		echo "USER_EXISTS";
-    } else {
-		$stmt = $conn->prepare("INSERT INTO CLIENT (email,phoneNumber,idNumber,password,age,firstName,middleName,lastName) VALUES (?,?,?,?,?,?,?,?)");
+    } elseif ($check_secret_key['RESULT'] == '0') {
+		echo "INVALID_KEY";
+	} else	{
+		$stmt = $conn->prepare("INSERT INTO ADMIN (email,phoneNumber,idNumber,password,age,firstName,middleName,lastName) VALUES (?,?,?,?,?,?,?,?)");
 		$stmt->bind_param("ssssisss", $email, $phoneNum, $idNum, $password, $age, $firstName, $middleName, $lastName);
 		$stmt->execute();
 		
