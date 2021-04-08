a7d71e577ae1a504d993e0cd38ad84abb90263d1
sheslin25 <2094701@students.wits.ac.za>
2021-04-08 12:44:37 +0200

    Changed imports
    Flutter runs normal at this point

--- lib/core/registration/registration.functions.dart
+++ lib/core/registration/registration.functions.dart
@@ -3,15 +3,15 @@ import 'dart:core';
 import 'dart:html';
 
 import 'package:flutter/material.dart';
-import 'package:last_national_bank/widgets/NewAge.dart';
-import 'package:last_national_bank/widgets/NewSurname.dart';
-import 'package:last_national_bank/widgets/newName.dart';
-import 'package:last_national_bank/widgets/newEmail.dart';
-import 'package:last_national_bank/widgets/NewIDnum.dart';
-import 'package:last_national_bank/widgets/NewLoc.dart';
-import 'package:last_national_bank/widgets/NewPhone.dart';
-import 'package:last_national_bank/widgets/NewPassword.dart';
-import 'package:last_national_bank/widgets/NewPassword2.dart';
+import 'package:last_national_bank/core/registration/widgets/NewAge.dart';
+import 'package:last_national_bank/core/registration/widgets/NewIDnum.dart';
+import 'package:last_national_bank/core/registration/widgets/NewLoc.dart';
+import 'package:last_national_bank/core/registration/widgets/NewPassword.dart';
+import 'package:last_national_bank/core/registration/widgets/NewPassword2.dart';
+import 'package:last_national_bank/core/registration/widgets/NewPhone.dart';
+import 'package:last_national_bank/core/registration/widgets/NewSurname.dart';
+import 'package:last_national_bank/core/registration/widgets/newEmail.dart';
+import 'package:last_national_bank/core/registration/widgets/newName.dart';
 import 'package:last_national_bank/core/SHA-256_encryption.dart';
 
 // bool registerAdmin(String idNumber) {
@@ -23,8 +23,8 @@ var surname = NewSurnameState().returnSurName();
 var email = NewEmailState().returnEmail();
 var idNum = NewIDnumState().returnID();
 var loc = NewLocState().returnloc();
-var password = "";//PasswordInputState().getPassword();
-var password2 = "";//PasswordInput2State().returnpassword() ;
+var password = PasswordInputState().getPassword();
+var password2 = PasswordInput2State().returnpassword2() ;
 var phone = NewPhoneState().returnPhone();
 
 var hash = encode(password);
