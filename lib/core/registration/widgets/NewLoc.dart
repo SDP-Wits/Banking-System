0892c033f35e108c3fd3acb6fbb21b6cda391f1e
moeGathoo <migathoo@outlook.com>
2021-04-10 22:54:18 +0200

    Added multiple address fields
    Added multiple address fields to simplify adding to database.

--- lib/core/registration/widgets/NewLoc.dart
+++ lib/core/registration/widgets/NewLoc.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 import '../registration.functions.dart';
@@ -25,32 +27,144 @@ class NewLocState extends State<NewLoc> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return Container(
       padding: const EdgeInsets.only(top: 20, left: 50, right: 50),
       child: Container(
-        height: 60,
+        height: 300,
         width: MediaQuery.of(context).size.width,
-        child: TextField(
-          onChanged: (value) {
-            _hasInputError =
-                hasInputError(value); //call validator to check for errors
-            if (_hasInputError == false) {
-              //if no errors, assign location
-              _loc = value;
-              Data.loc = value;
-            }
-            setState(() {});
-          },
-          style: TextStyle(
-            color: Colors.white,
-          ),
-          decoration: InputDecoration(
-            errorText: _hasInputError ? "Invlaid Location" : null,
-            fillColor: Colors.transparent,
-            hintText: 'Location',
-            hintStyle: TextStyle(fontSize: 16.0, color: Colors.white),
-            icon: Icon(Icons.location_pin, color: Colors.white),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Addresses',
+              textAlign: TextAlign.left,
+              style: TextStyle(fontSize: 16.0, color: Colors.white),
+            ),
+            TextField(
+              scrollPadding:
+                  const EdgeInsets.only(top: 30, left: 50, right: 50),
+              onChanged: (value) {
+                _hasInputError =
+                    hasInputError(value); //call validator to check for errors
+                if (_hasInputError == false) {
+                  //if no errors, assign location
+                  _loc = value;
+                  Data.loc = value;
+                }
+                setState(() {});
+              },
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                /*contentPadding:
+                    const EdgeInsets.only(top: 30, left: 50, right: 50),*/
+                errorText: _hasInputError ? "Invlaid Location" : null,
+                fillColor: Colors.transparent,
+                hintText: 'Street Number',
+                hintStyle: TextStyle(fontSize: 16.0, color: Colors.white),
+                icon: Icon(Icons.location_pin, color: Colors.white),
+              ),
+            ),
+            TextField(
+              scrollPadding:
+                  const EdgeInsets.only(top: 30, left: 50, right: 50),
+              onChanged: (value) {
+                _hasInputError =
+                    hasInputError(value); //call validator to check for errors
+                if (_hasInputError == false) {
+                  //if no errors, assign location
+                  _loc = value;
+                  Data.loc = value;
+                }
+                setState(() {});
+              },
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                /*contentPadding:
+                    const EdgeInsets.only(top: 30, left: 50, right: 50),*/
+                errorText: _hasInputError ? "Invlaid Location" : null,
+                fillColor: Colors.transparent,
+                hintText: 'Street Name',
+                hintStyle: TextStyle(fontSize: 16.0, color: Colors.white),
+                icon: Icon(Icons.location_pin, color: Colors.white),
+              ),
+            ),
+            TextField(
+              scrollPadding:
+                  const EdgeInsets.only(top: 30, left: 50, right: 50),
+              onChanged: (value) {
+                _hasInputError =
+                    hasInputError(value); //call validator to check for errors
+                if (_hasInputError == false) {
+                  //if no errors, assign location
+                  _loc = value;
+                  Data.loc = value;
+                }
+                setState(() {});
+              },
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                errorText: _hasInputError ? "Invlaid Location" : null,
+                fillColor: Colors.transparent,
+                hintText: 'Suburb',
+                hintStyle: TextStyle(fontSize: 16.0, color: Colors.white),
+                icon: Icon(Icons.location_pin, color: Colors.white),
+              ),
+            ),
+            TextField(
+              scrollPadding:
+                  const EdgeInsets.only(top: 30, left: 50, right: 50),
+              onChanged: (value) {
+                _hasInputError =
+                    hasInputError(value); //call validator to check for errors
+                if (_hasInputError == false) {
+                  //if no errors, assign location
+                  _loc = value;
+                  Data.loc = value;
+                }
+                setState(() {});
+              },
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                errorText: _hasInputError ? "Invlaid Location" : null,
+                fillColor: Colors.transparent,
+                hintText: 'Province',
+                hintStyle: TextStyle(fontSize: 16.0, color: Colors.white),
+                icon: Icon(Icons.location_pin, color: Colors.white),
+              ),
+            ),
+            TextField(
+              scrollPadding:
+                  const EdgeInsets.only(top: 30, left: 50, right: 50),
+              onChanged: (value) {
+                _hasInputError =
+                    hasInputError(value); //call validator to check for errors
+                if (_hasInputError == false) {
+                  //if no errors, assign location
+                  _loc = value;
+                  Data.loc = value;
+                }
+                setState(() {});
+              },
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                errorText: _hasInputError ? "Invlaid Location" : null,
+                fillColor: Colors.transparent,
+                hintText: 'Apartment number',
+                hintStyle: TextStyle(fontSize: 16.0, color: Colors.white),
+                icon: Icon(Icons.location_pin, color: Colors.white),
+              ),
+            ),
+          ],
         ),
       ),
     );
