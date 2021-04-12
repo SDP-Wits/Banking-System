0bee20e41103b3d98c8b4b03ed09e039715c58f8
Arneev <>
2021-04-12 12:04:07 +0200

    tests update

--- test/php_request.test.dart
+++
@@ -1,6 +0,0 @@
-// import 'package:last_national_bank/utils/services/online_db.dart';
-
-// Future<void> main() async {
-//   print("Test php data is (in raw format): ");
-//   print((await getTestPHPData()).toString());
-// }
