4a288e604308a682874404cc58615715c20d489e
Arneev <arneevso@gmail.com>
2021-04-29 23:25:05 +0200

    HTTP Request, Account Card Info
    HTTP Request for Accept/Reject Client
    
    Did some UI for the account card info

--- lib/utils/helpers/style.dart
+++ lib/utils/helpers/style.dart
@@ -12,3 +12,9 @@ final double fontSizeSmall = 20.0;
 
 final Color primaryCol = Colors.black;
 final Color secondaryCol = Colors.red;
+
+var backgroundGradient = LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [Colors.blueGrey, Colors.teal]);
+
