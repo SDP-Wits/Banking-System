91ecc3df3c87f0bd21958997808be00e29e3b987
NicoGovStudent <69640301+NicoGovStudent@users.noreply.github.com>
2021-05-09 13:47:03 +0200

    verify_client.php changes

--- Planning/Files/php/verify_client.php
+++ Planning/Files/php/verify_client.php
@@ -5,12 +5,12 @@ $clientIdNum = $_REQUEST["clientIdNum"];
 $adminIdNum = $_REQUEST["adminIdNum"];
 $currentDate = $_REQUEST["currentDate"];
 
-/*
-$clientIdNum = '8';
-$adminIdNum = '42';
-$currentDate = '2021-04-14';
-*/
+//test example
+/*$clientIdNum = '0070070070071';
+$adminIdNum = '0000033999999';
+$currentDate = '2021-04-14';*/
 
+//get table IDs of client and admin by querying with actual ID numbers
 $sql1 = "SELECT adminID AS ID FROM ADMIN WHERE idNumber = '$adminIdNum'";
 $sql2 = "SELECT clientID AS ID FROM CLIENT WHERE idNumber = '$clientIdNum'";
 
@@ -21,16 +21,17 @@ if($adminCheck and $clientCheck) {
 	$check_admin = mysqli_fetch_array($adminCheck);
 	$check_client = mysqli_fetch_array($clientCheck);
 	
+        //insert entry into VERIFIED CLIENTS therefore updating client status to verified
 	$stmt = $conn->prepare("INSERT INTO `VERIFIED CLIENT` (clientID,verifiedDate,verifiedBy) VALUES (?,?,?)");
 	$stmt->bind_param("isi",$check_client['ID'],$currentDate,$check_admin['ID']);
 	$stmt->execute();
-
-	//I added this code here to return status true so we know it was successful
-	echo json_encode(
-		array(
-			array("status" => TRUE)
-		)
-	);
+        
+        //I added this code here to return status true so we know it was successful
+        echo json_encode(
+            array(
+                array("status" => TRUE)
+            )
+        );
 
 } else {
 	echo json_encode(
