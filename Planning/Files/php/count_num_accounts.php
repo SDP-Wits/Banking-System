61376d52c792249913d391776e0f3986aaa8817c
ShravanSingh2 <2173638@students.wits.ac.za>
2021-05-15 15:54:45 -0700

    Create count_num_accounts.php

---
+++ Planning/Files/php/count_num_accounts.php
@@ -0,0 +1,28 @@
+<?php
+include "./helpers/server_details.php";
+
+$sql = "SELECT COUNT(*) FROM `ACCOUNT TYPE`";
+
+$result = mysqli_query($conn,$sql);
+
+if (mysqli_num_rows($result) < 1) { 
+  echo json_encode(
+    array(
+      array(
+        "status" => FALSE, "error" => "Account types table is Empty"
+      )
+    )
+  );
+  return;
+ }
+
+$output = array();
+
+while ($row=$result->fetch_assoc()){
+        $output[]=$row;
+}
+
+echo json_encode($output);
+
+$conn->close();
+?>
