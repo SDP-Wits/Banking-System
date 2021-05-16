02964f60b81ad3c12d97c2104e0209f80dfa3082
TristanBookhan <65284370+TristanBookhan@users.noreply.github.com>
2021-05-16 16:57:51 +0200

    refined output for php file

--- Planning/Files/php/select_specific_account.php
+++ Planning/Files/php/select_specific_account.php
@@ -3,13 +3,12 @@ include "./helpers/server_details.php";
 
 $accNum = $_REQUEST['accNum'];
 
-$sql = "SELECT * 
+$sql = "SELECT ACCOUNT.accountNumber,ACCOUNT.accountTypeID,`ACCOUNT TYPE`.accountType,`ACCOUNT TYPE`.accountDescription,TRANSACTION
 FROM ACCOUNT
-INNER JOIN `CLIENT-ACCOUNT`ON ACCOUNT.accountNumber=`CLIENT-ACCOUNT`.accountNumber
 INNER JOIN `ACCOUNT TYPE` ON `ACCOUNT TYPE`.accountTypeID=ACCOUNT.AccountTypeID
-INNER JOIN LOG ON LOG.clientID = `CLIENT-ACCOUNT`.clientID
 INNER JOIN TRANSACTION ON TRANSACTION.accountFrom = ACCOUNT.accountNumber OR TRANSACTION.accountTo = ACCOUNT.accountNumber
-WHERE ACCOUNT.accountNumber = '$accNum'";
+WHERE ACCOUNT.accountNumber = '$accNum'
+GROUP BY TRANSACTION.transactionID";
 
 $result = mysqli_query($conn,$sql);
 
