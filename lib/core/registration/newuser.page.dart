5d4c7b0ca02cb78e4acd10026c8ce061a2e916a9
TristanBookhan <65284370+TristanBookhan@users.noreply.github.com>
2021-05-23 18:00:06 +0200

    removed coverage removal test

--- lib/core/registration/newuser.page.dart
+++ lib/core/registration/newuser.page.dart
@@ -1,58 +1,58 @@
-// TODO:turn this page into selector for admin or customer registration //# nocov
+// TODO:turn this page into selector for admin or customer registration 
 
-import 'package:flutter/material.dart';//# nocov
-import 'package:last_national_bank/core/registration/widgets/Logo.dart';//# nocov
-import 'package:last_national_bank/core/registration/widgets/NewAge.dart';//# nocov
-import 'package:last_national_bank/core/registration/widgets/NewIDnum.dart';//# nocov
-import 'package:last_national_bank/core/registration/widgets/NewLoc.dart';//# nocov
-import 'package:last_national_bank/core/registration/widgets/NewPassword.dart';//# nocov
-import 'package:last_national_bank/core/registration/widgets/NewSurname.dart';//# nocov
-import 'package:last_national_bank/core/registration/widgets/newEmail.dart';//# nocov
-import 'package:last_national_bank/core/registration/widgets/newName.dart';//# nocov
-import 'package:last_national_bank/widgets/Secret.dart';//# nocov
-import 'package:last_national_bank/widgets/buttonNewUser.dart';//# nocov
-import 'package:last_national_bank/widgets/userOld.dart';//# nocov
+import 'package:flutter/material.dart';
+import 'package:last_national_bank/core/registration/widgets/Logo.dart';
+import 'package:last_national_bank/core/registration/widgets/NewAge.dart';
+import 'package:last_national_bank/core/registration/widgets/NewIDnum.dart';
+import 'package:last_national_bank/core/registration/widgets/NewLoc.dart';
+import 'package:last_national_bank/core/registration/widgets/NewPassword.dart';
+import 'package:last_national_bank/core/registration/widgets/NewSurname.dart';
+import 'package:last_national_bank/core/registration/widgets/newEmail.dart';
+import 'package:last_national_bank/core/registration/widgets/newName.dart';
+import 'package:last_national_bank/widgets/Secret.dart';
+import 'package:last_national_bank/widgets/buttonNewUser.dart';
+import 'package:last_national_bank/widgets/userOld.dart';
 
-class NewUser extends StatefulWidget {//# nocov
-  @override//# nocov
-  _NewUserState createState() => _NewUserState();//# nocov
-}//# nocov
+class NewUser extends StatefulWidget {
+  @override
+  _NewUserState createState() => _NewUserState();
+}
 
-class _NewUserState extends State<NewUser> {//# nocov
-  @override//# nocov
-  Widget build(BuildContext context) {//# nocov
-    return Container(//# nocov
-      decoration: BoxDecoration(//# nocov
-        gradient: LinearGradient(//# nocov
-            begin: Alignment.topCenter,//# nocov
-            end: Alignment.bottomCenter,//# nocov
-            colors: [Colors.blueGrey, Colors.teal]),//# nocov
-      ),//# nocov
-      child: ListView(//# nocov
-        children: <Widget>[//# nocov
-          Column(//# nocov
-            children: <Widget>[//# nocov
-              Row(//# nocov
-                mainAxisAlignment: MainAxisAlignment.center,//# nocov
-                children: <Widget>[//# nocov
-                  Logo(),//# nocov
-                  //TextNew(),//# nocov
-                ],//# nocov
-              ),//# nocov
-              NewName(),//# nocov
-              NewSurname(),//# nocov
-              NewAge(),//# nocov
-              NewEmail(),//# nocov
-              NewIDnum(),//# nocov
-              NewLoc(),//# nocov
-              PasswordInput(),//# nocov
-              SecretKey(),//# nocov
-              ButtonNewUser(),//# nocov
-              UserOld(),//# nocov
-            ],//# nocov
-          ),//# nocov
-        ],//# nocov
-      ),//# nocov
-    );//# nocov
-  }//# nocov
-}//# nocov
+class _NewUserState extends State<NewUser> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blueGrey, Colors.teal]),
+      ),
+      child: ListView(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Logo(),
+                  //TextNew(),
+                ],
+              ),
+              NewName(),
+              NewSurname(),
+              NewAge(),
+              NewEmail(),
+              NewIDnum(),
+              NewLoc(),
+              PasswordInput(),
+              SecretKey(),
+              ButtonNewUser(),
+              UserOld(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
