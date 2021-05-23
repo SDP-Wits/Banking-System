bd9c0fc366cb5bbc5e712d963150f00a995e22c5
Arneev <arneevso@gmail.com>
2021-05-24 01:26:26 +0200

    Update login.functions.dart

--- lib/core/login/login.functions.dart
+++ lib/core/login/login.functions.dart
@@ -1,3 +1,4 @@
+// coverage:ignore-start
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
@@ -7,99 +8,75 @@ import '../../constants/database_constants.dart';
 import '../../utils/services/online_db.dart';
 import '../SHA-256_encryption.dart';
 
-//coverage:ignore-start
-final TextEditingController idController = //coverage:ignore-line
-    TextEditingController(text: ""); //coverage:ignore-line
-final TextEditingController passwordController = //coverage:ignore-line
-    TextEditingController(text: ""); //coverage:ignore-line
+final TextEditingController idController = TextEditingController(text: "");
+final TextEditingController passwordController =
+    TextEditingController(text: "");
 
-TextEditingController getIDController() { //coverage:ignore-line
-  return idController; //coverage:ignore-line
+TextEditingController getIDController() {
+  return idController;
 }
 
-TextEditingController getPasswordController() {//coverage:ignore-line
-  return passwordController; //coverage:ignore-line
+TextEditingController getPasswordController() {
+  return passwordController;
 }
 
 Future<void> loginProcedure(BuildContext context) async {
-  //coverage:ignore-line
-  bool isClientLogin = true; //coverage:ignore-line
+  bool isClientLogin = true;
   await showDialog(
-      //coverage:ignore-line
-      context: context, //coverage:ignore-line
-      barrierDismissible: false, //coverage:ignore-line
-      builder: (BuildContext context) {//coverage:ignore-line
-        return AlertDialog( //coverage:ignore-line
-          title: Center(  //coverage:ignore-line
-            child: Text("Login?"), //coverage:ignore-line
-          ), //coverage:ignore-line
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Center(
+            child: Text("Login?"),
+          ),
           content: Row(
-            //coverage:ignore-line
-            mainAxisAlignment: MainAxisAlignment.center, //coverage:ignore-line
-            crossAxisAlignment:
-                CrossAxisAlignment.center, //coverage:ignore-line
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              //coverage:ignore-line
               Expanded(
-                //coverage:ignore-line
                 child: Text(
-                  //coverage:ignore-line
-                  "Do you want to login in with your Admin account", //coverage:ignore-line
-                  textAlign: TextAlign.start, //coverage:ignore-line
-                ), //coverage:ignore-line
-              ), //coverage:ignore-line
-            ], //coverage:ignore-line
-          ), //coverage:ignore-line
+                  "Do you want to login in with your Admin account",
+                  textAlign: TextAlign.start,
+                ),
+              ),
+            ],
+          ),
           actions: <Widget>[
-            //coverage:ignore-line
             FlatButton(
-              //coverage:ignore-line
-              child: Text('Yes'), //coverage:ignore-line
+              child: Text('Yes'),
               onPressed: () {
-                //coverage:ignore-line
-                isClientLogin = false; //coverage:ignore-line
-                Navigator.pop(context); //coverage:ignore-line
-              }, //coverage:ignore-line
-            ), //coverage:ignore-line
+                isClientLogin = false;
+                Navigator.pop(context);
+              },
+            ),
             FlatButton(
-              //coverage:ignore-line
-              child: Text('No'), //coverage:ignore-line
-              onPressed: () {//coverage:ignore-line
-                isClientLogin = true; //coverage:ignore-line
-                Navigator.pop(context); //coverage:ignore-line
-              }, //coverage:ignore-line
-            ), //coverage:ignore-line
-          ], //coverage:ignore-line
-        ); //coverage:ignore-line
-      }); //coverage:ignore-line
+              child: Text('No'),
+              onPressed: () {
+                isClientLogin = true;
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      });
 
-  String id = idController.text; //coverage:ignore-line
-  String password = encode(passwordController.text); //coverage:ignore-line
+  String id = idController.text;
+  String password = encode(passwordController.text);
 
-  String response =
-      await userLoginOnline(id, password, isClientLogin); //coverage:ignore-line
-  Fluttertoast.showToast(msg: response); //coverage:ignore-line
+  String response = await userLoginOnline(id, password, isClientLogin);
+  Fluttertoast.showToast(msg: response);
 
   if (response == dbSuccess) {
-    //coverage:ignore-line
-    currID.id = idController.text; //coverage:ignore-line
+    currID.id = idController.text;
     if (!isClientLogin) {
-      //coverage:ignore-line
-      goToAdminVerificationList(context); //coverage:ignore-line
+      goToAdminVerificationList(context);
     } else {
-      //coverage:ignore-line
-      goToAdminVerificationStatus(context); //coverage:ignore-line
-      // getAccountDetails(id).then((accountDetails) {
-      //   if (accountDetails.isEmpty) {
-      //     goToAdminVerificationStatus(context);
-      //   } else {
-      //     goToViewAccount(context);
-      //   }
-      // });
+      goToAdminVerificationStatus(context);
     }
   }
 }
-//coverage:ignore-end
+// coverage:ignore-end
 
 //Helper Functions
 
