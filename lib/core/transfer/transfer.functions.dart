fc5e89c64813ee9c5e267a1315d2f9e8763ef4dc
NicoGovStudent <69640301+NicoGovStudent@users.noreply.github.com>
2021-06-03 18:12:55 +0200

    transfer http and extraction

---
+++ lib/core/transfer/transfer.functions.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:last_national_bank/utils/services/online_db.dart';
+
+//Text for inputs
+String accountToText = "";
+String accountFromText = "";
+String amountText = "";
+String referenceNameText = "";
+
+//Controllers for input fields
+final TextEditingController accountToController =
+TextEditingController(text: accountToText);
+
+final TextEditingController accountFromController =
+TextEditingController(text: accountFromText);
+
+final TextEditingController amountController =
+TextEditingController(text: amountText);
+
+final TextEditingController referenceNameController =
+TextEditingController(text: referenceNameText);
+
+//onChange for Controllers
+void onChangeAccountTo(String newAccountTo) {
+  accountToText = newAccountTo;
+}
+
+void onChangeAccountFrom(String newAccountFrom) {
+  accountFromText = newAccountFrom;
+}
+
+void onChangeAmount(String newAmount) {
+  amountText = newAmount;
+}
+
+void onChangeReferenceName(String newReferenceName) {
+  referenceNameText = newReferenceName;
+}
+
+//Other functions
+void submitTransfer(){
+  //validation checks
+  int amount;
+
+  //check if input amount is correct
+  try {
+    amount = int.parse(amountText);
+  } catch (e) {
+    Fluttertoast.showToast(msg: "Please Enter Valid Amount");
+    return;
+  }
+
+  //check if account number is valid
+  if (accountToText.length != 11 || accountFromText.length != 11) {
+    Fluttertoast.showToast(msg: "Please Enter Valid Account Number/s");
+    return;
+  }
+
+  //http request to make transfer when send button is pressed
+  makeTransfer(accountFromText,accountToText,amountText,referenceNameText);
+
+}
