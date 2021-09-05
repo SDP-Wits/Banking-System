da662b18742186a91140bb462c0b24f963043276
Nico Jared Govindsamy <ngovindsamy18@gmail.com>
2021-09-05 03:07:33 +0200

    created insert secret key php

---
+++ Planning/Files/php/insert_secret_key.php
@@ -0,0 +1,24 @@
+<?php
+include "./helpers/server_details.php";
+include "./helpers/encryption.php";
+
+//FOR BACK-END USE ONLY!!!
+
+//create new key here - it gets encrypted
+$date = "2021-09-05"; //test date - make current date
+$newKey = "Testing1234";
+$newKey = openssl_encrypt($newKey, $ciphering, $encryption_key, $options, $encryption_iv);
+
+//insert into online db
+$stmt = $conn->prepare("INSERT INTO `SECRET KEY` (secretKey,creationDate) VALUES (?,?)");
+$stmt->bind_param("ss", $newKey, $date);
+$stmt->execute();
+
+//decrypt check
+$decrypt = openssl_decrypt($newKey, $ciphering, $decryption_key, $options, $decryption_iv);
+echo $decrypt;
+	
+
+$conn->close();
+
+?>
